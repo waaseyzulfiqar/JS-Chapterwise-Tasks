b4ad5362ce3a55b7560b9ffc03026f358bb63ad4
waaseyzulfiqar <wasey03112920792@gmail.com>
2024-02-19 10:38:50 +0500

    JS tasks from chapter 1-5

---
+++ app.js
@@ -0,0 +1,370 @@
+// ************** chapter 01 Tasks **************
+
+// Question 01 //
+
+// alert("Welcome to Our website");
+
+// Question 02 //
+
+// alert("Error! Please Enter a Valid Password");
+
+// Question 03 //
+
+// alert("Welcome to JS Land... \nHappy Coding!")
+
+// Question 04 //
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!..")
+
+// Question 05 // 
+
+// completed in console ✅
+
+// Question 06 //
+
+// no change occurs on any position of script ✅
+
+
+// ************** chapter 02 Tasks **************
+
+
+// Question 01 //
+
+// var username;
+
+
+// Question 02 //
+
+// var myname = "Waasey Zulfiqar";
+
+
+// Question 03 //
+
+// var message = "Hello World";
+// alert(message);
+
+// Question 04 //
+
+// var name = "Jhone Doe";
+// var age = 15;
+// var course = "Certified Mobile Application Development";
+
+// alert(name);
+// alert(age + " years old");
+// alert(course);
+
+// Question 05 //
+
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza);
+
+// Question 06 //
+
+// var email = "wasey03112920792@gmail.com";
+// alert("My Email Address is " + email);
+
+// Question 07 //
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// Question 08 //
+// document.write('Yah!..I can write HTML Content through JavaScript');
+
+// Question 09 //
+// var ajeeb = "_____________ஜ۩۞۩ஜ______________";
+// alert(ajeeb);
+
+
+// ************** chapter 03 Tasks **************
+
+// Question 01 //
+
+// var age = 20;
+// alert("I am " + age + " years old"); 
+
+// Question 02 //
+
+// var websiteVisit = 25;
+// alert("You have visited this Website " + websiteVisit + " times");
+
+// Question 03 //
+
+// var birthYear = 2004;
+// document.write("My Birth Year is " + birthYear + "<br>");
+
+// var type = typeof(birthYear);
+// document.write("Datatype of my declared variable is " + type);
+
+// Question 04 //
+
+// var visitorName = "Waasey Zulfiqar";
+// var productTitle = "Lexus LX-570";
+// var Quantity = 6;
+
+// document.write(visitorName + " Booked " + Quantity + " " + productTitle);
+
+
+// ************** chapter 04 Tasks **************
+
+
+// Question 01 //
+
+// var name , age , email;
+
+// Question 02 //
+
+// illegal variables
+
+// var 2you;
+// var user name;
+// var prompt;
+// var username;
+
+// legal variables
+
+// var user3;
+// var $name;
+// var _name;
+// var myAlert;
+// var myAge; (camelCase)
+// var Email;
+
+// Question 03 //
+
+// not done ❌
+
+
+// ************** chapter 05 Tasks **************
+
+
+// Question 01 //
+
+
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+
+// var num3 = num1 + num2;
+
+// document.write("Sum of " + num1 + " and " + num2 + " is " + num3);
+
+
+// Question 02 //
+
+
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+
+// var num3 = num1 - num2;
+
+// document.write("Subtraction of " + num1 + " and " + num2 + " is " + num3);
+
+
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+
+// var num3 = num1 * num2;
+
+// document.write("Product of " + num1 + " and " + num2 + " is " + num3);
+
+
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+
+// var num3 = num1 / num2;
+
+// document.write("Division of " + num1 + " and " + num2 + " is " + num3);
+
+
+// var num1 = +prompt("Enter First Number");
+// var num2 = +prompt("Enter Second Number");
+
+// var num3 = num1 % num2;
+
+// document.write("Modulus of " + num1 + " and " + num2 + " is " + num3);
+
+
+// Question 03 //
+
+
+// var number;
+
+// document.write("Value after variable declaration is " + number);
+
+// number = 5;
+
+// document.write("<br>Initial Value : " + number);
+
+// number++;
+
+// document.write("<br>Value after increment is : " + number);
+
+// var addition = number + 7;
+
+// document.write("<br>Value after Addition is : " + addition);
+
+// addition--;
+
+// document.write("<br>Value after decrement is : " + addition);
+
+// var remainder = addition % 3;
+
+// document.write("<br>The Remainder is : " + remainder);
+
+
+// Question 04 //
+
+
+// var ticketPrice = 600;
+
+// var ticketBuyer = +prompt("How many tickets you want..?")
+
+// var finalPrice = ticketBuyer * ticketPrice;
+
+// document.write("The Total Cost to buy " + ticketBuyer + " tickets to a movie is " + finalPrice);
+
+
+// Question 05 //
+
+// var table = 2
+
+// document.write("2 * 1 = " + table * 1)
+// document.write("<br>2 * 2 = " + table * 2)
+// document.write("<br>2 * 3 = " + table * 3)
+// document.write("<br>2 * 4 = " + table * 4)
+// document.write("<br>2 * 5 = " + table * 5)
+// document.write("<br>2 * 6 = " + table * 6)
+// document.write("<br>2 * 7 = " + table * 7)
+// document.write("<br>2 * 8 = " + table * 8)
+// document.write("<br>2 * 9 = " + table * 9)
+// document.write("<br>2 * 10 = " + table * 10)
+
+// Question 06 //
+
+// var temperatureInCentigrade = +prompt("Enter outside temperature in Celcius");
+
+// var temperatureInFahrenheit = +prompt("Enter outside temperature in Fahrenheit")
+
+
+// var fahrenheit = (temperatureInCentigrade * 9/5) + 32;
+
+// var celcius = (temperatureInFahrenheit - 32) * 5/9;
+
+// document.write( temperatureInCentigrade + "C" + " is " + fahrenheit + "F")
+
+// document.write("<br>" + temperatureInFahrenheit + "F" + " is " + celcius + "C")
+
+
+// Question 07 //
+
+// var item1Price = +prompt("Enter item 1 price");
+
+// var quantityOfItem1 = +prompt("Enter Quantity of item 1");
+
+// var item2Price = +prompt("Enter item 2 price");
+
+// var quantityOfItem2 = +prompt("Enter Quantity of item 2");
+
+
+// var totalPrice = (item1Price * quantityOfItem1) + (item2Price * quantityOfItem2) + 100;
+
+
+// document.write("Price of item 1 is " + item1Price);
+
+// document.write("<br>Quantity of item 1 is " + quantityOfItem1);
+
+// document.write("<br>Price of item 2 is " + item2Price);
+
+// document.write("<br>Quantity of item 2 is " + quantityOfItem2);
+
+// document.write("<br>Shipping charges 100");
+
+// document.write("<br>Total cost of your order is " +totalPrice);
+
+
+// Question 08 //
+
+// var totalMarks = +prompt("Enter your Total Marks..");
+
+// var marksObtained = +prompt("Enter your Obtained Marks...");
+
+// var percentage = (marksObtained * 100) / totalMarks;
+
+// document.write("Total Marks : " + totalMarks);
+
+// document.write("<br>Marks Obtained : " + marksObtained);
+
+// document.write("<br>Percentage : " + percentage);
+
+
+// Question 09 //
+
+// var dollars = +prompt("Tell me how many dollars do you have...?");
+
+// var riyal = +prompt("How many Riyals do you have...?");
+
+// var usd = dollars * 104.80;
+
+// var saudi = riyal * 28;
+
+
+// var totalRupees = (usd + saudi);
+
+// document.write("Total Currency in PKR is : " + totalRupees);
+
+
+// Question 10 //
+
+// var num = ((5 + 5) * 10) / 2;
+
+// Question 11 //
+
+// var birthYear = +prompt("Enter you Birth Year");
+
+// var year = 2024;
+
+// var age = year - birthYear;
+
+// document.write("Birth Year is : " + birthYear);
+
+// document.write("<br>You are " + age + " years old ");
+
+
+// Question 12 //
+
+// var radiusOfCircle = +prompt("Enter your object radius");
+
+// var circumferenceOfCircle = radiusOfCircle * 2 * 3.142;
+
+// var areaOfCircle = radiusOfCircle * radiusOfCircle * 3.142
+
+// document.write("Radius of Circle is : " + radiusOfCircle);
+
+// document.write("<br>The Circumference is : " + circumferenceOfCircle);
+
+// document.write("<br>The Area is : " + areaOfCircle);
+
+
+// Question 13 //
+
+
+// var snack = prompt("Enter your Favorite Sanck");
+
+// var inADay = +prompt("How many times in a day you have your favorite snack");
+
+// var minAge = +prompt("Enter Your Age");
+
+// var maxAge = 100;
+
+// var totalSnacks = (inADay * 365) * (maxAge - minAge);
+
+// document.write("Favorite Snack : " + snack);
+
+// document.write("<br>Current Age : " + minAge);
+
+// document.write("<br>Estimated Maximum Age : " + maxAge);
+
+// document.write("<br>Amount of snacks per day : " + inADay);
+
+// document.write("<br>You will need " + totalSnacks + " " + snack + " to last you until the age of " + maxAge);
